b9c16ea23be18d37d04a6a681d3cb5d3a24b2ffd
genzj <zj0512@gmail.com>
2024-11-28 11:44:10 -0800

    feat: :sparkles: auto install fd-find

--- src/run_once_10-install-tools.zsh
+++ src/run_once_10-install-tools.zsh
@@ -9,4 +9,17 @@ set -x
 }
 # )))
 
+# Install fd (((
+() {
+    (( ${+commands[fd]} )) && return 0
+    if (( ${+commands[fdfind]} )) ; then \
+        mkdir -p "$HOME/.local/bin"
+        [[ -f "$HOME/.local/bin/fd" ]] || ln -s "${commands[fdfind]}" "$HOME/.local/bin/fd"
+        return 0
+    fi
+    /usr/bin/env | sort
+    "${CHEZMOI_SOURCE_DIR}/../scripts/fd-install.sh" --bin-dir ~/.local/bin
+}
+# )))
+
 # vim: set foldenable foldmethod=marker foldlevel=0 foldmarker=(((,))):
