dd88bbff84b7942a82b82f0c82afaaaadb28eb14
genzj <zj0512@gmail.com>
2024-12-16 14:31:49 -0800

    fix: :bug: fix ubuntu-zsh detect for legacy Ubuntu releases

--- src/run_after_99-post-actions.zsh
+++ src/run_after_99-post-actions.zsh
@@ -19,7 +19,8 @@ set -x
 # Disable global compinit if Zsh is installed by Ubuntu apt (((
 () {
     (( ${+commands[apt]} )) || return 0
-    apt list --installed zsh 2>/dev/null | grep -q ubuntu || return 0
+    [[ -f /etc/zsh/zshrc ]] || return 0
+    grep -q 'skip_global_compinit' /etc/zsh/zshrc 2>/dev/null || return 0
     grep -q 'skip_global_compinit' ~/.zshenv 2>/dev/null && return 0
     echo 'skip_global_compinit=1' >>! ~/.zshenv
 }
