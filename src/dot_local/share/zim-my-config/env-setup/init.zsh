b7fb4738704f49b39e713a6f54f170403b0db1e1
Jie Zhu <zhuje@amazon.com>
2025-09-23 11:06:09 -0700

    feat: :sparkles: add a git show-branch alias

--- src/dot_local/share/zim-my-config/env-setup/init.zsh
+++ src/dot_local/share/zim-my-config/env-setup/init.zsh
@@ -47,6 +47,7 @@
     alias Gl1='git log -1 --date-order --pretty=format:"${_git_log_fuller_format}"'
     alias Gmff='git merge --ff-only'
     alias Gw='git status'
+    alias GbS='git show-branch --all --sha1-name'
     # )))
 }
 # )))
