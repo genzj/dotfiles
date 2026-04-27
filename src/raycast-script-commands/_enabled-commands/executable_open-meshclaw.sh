bf6e8741e898ae25a528fd2626c99db7ac406708
genzj <zj0512@gmail.com>
2026-04-27 16:24:20 -0700

    chore: :hammer: Add optional parameters and icon for MeshClaw script

--- src/raycast-script-commands/_enabled-commands/executable_open-meshclaw.sh
+++ src/raycast-script-commands/_enabled-commands/executable_open-meshclaw.sh
@@ -4,5 +4,8 @@
 # @raycast.schemaVersion 1
 # @raycast.title Open MeshClaw
 # @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.icon 🦞
 
 open "$(ssh cloud-dsk "/bin/zsh -ic '~/workplace/MeshClaw/src/MeshClaw/bin/meshclaw token' 2>/dev/null")"
